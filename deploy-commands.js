4e2de2fbd27f2bca53246d68097985740ef68e81
Teemu Koski <80845642+TemeKoo@users.noreply.github.com>
2022-09-01 20:11:40 +0300

    First version

---
+++ deploy-commands.js
@@ -0,0 +1,14 @@
+const { SlashCommandBuilder, Routes } = require("discord.js");
+const { REST } = require("@discordjs/rest");
+const { clientId, guildId, token } = require("./config.json");
+
+const commands = [
+    new SlashCommandBuilder().setName("test").setDescription("This is a test command")
+]
+    .map(command => command.toJSON());
+
+const rest = new REST({ version: "10" }).setToken(token);
+
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+    .then((data) => console.log(`Successfully registered ${data.length} application commands.`))
+    .catch(console.error);
